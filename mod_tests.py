11fc56451ce368101b38cc99b4a6495bf0a4a13e
PhilippPrasse <philipp.prasse@bristol.ac.uk>
2016-06-30 15:51:26 +0100

    new files

---
+++ mod_tests.py
@@ -0,0 +1,12 @@
+# This is for you to fill in!
+
+
+from mean import *
+
+
+
+def test_mean_1234():
+	num_list = [1, 2, 3]
+	calc_mean = mean(num_list)
+	expected_mean = 2
+	assert expected_mean == calc_mean
